24e6fefd7d4c7c095e1a0d8a0a9d0bf7d23c5c34
samolego <34912839+samolego@users.noreply.github.com>
2021-02-28 14:51:59 +0100

    Temp removal of registry sync disabler

--- src/main/java/org/samo_lego/taterzens/mixin/RegistrySyncManagerMixin_TaterzenSyncDisabler.java
+++ src/main/java/org/samo_lego/taterzens/mixin/RegistrySyncManagerMixin_TaterzenSyncDisabler.java
@@ -1,12 +1,7 @@
 package org.samo_lego.taterzens.mixin;
 
 import net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager;
-import net.minecraft.nbt.CompoundTag;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-
-import static org.samo_lego.taterzens.Taterzens.MODID;
 
 @Mixin(RegistrySyncManager.class)
 public class RegistrySyncManagerMixin_TaterzenSyncDisabler {
@@ -15,24 +10,22 @@ public class RegistrySyncManagerMixin_TaterzenSyncDisabler {
      * Removes taterzen tag from registry sync, as we do not need it on client.
      * Prevents client from being kicked if using FAPI.
      */
-    @ModifyVariable(
-            method = "createPacket()Lnet/minecraft/network/Packet;",
+    /*@Inject(
+            method = "createPacket",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/network/PacketByteBuf;writeCompoundTag(Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/network/PacketByteBuf;"
             ),
+            locals = LocalCapture.CAPTURE_FAILHARD,
             remap = false
     )
-    private static CompoundTag removeTaterzenFromSync(CompoundTag tag) {
-        if(tag != null) {
-            CompoundTag registries = tag.getCompound("registries");
-            if(registries != null) {
-                CompoundTag entityTypes = registries.getCompound("minecraft:entity_type");
-                if(entityTypes != null) {
-                    entityTypes.remove(MODID + ":npc");
-                }
+    private static void removeTaterzenFromSync(CallbackInfoReturnable<Packet<?>> cir, CompoundTag tag, PacketByteBuf _buf) {
+        CompoundTag registries = tag.getCompound("registries");
+        if(registries != null) {
+            CompoundTag entityTypes = registries.getCompound("minecraft:entity_type");
+            if(entityTypes != null) {
+                entityTypes.remove(MODID + ":npc");
             }
         }
-        return tag;
-    }
+    }*/
 }
