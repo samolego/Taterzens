c7fdde28d09a0c169245896f4a4a81c707917434
samolego <34912839+samolego@users.noreply.github.com>
2021-10-01 15:57:55 +0200

    Minor changes

--- common/src/main/java/org/samo_lego/taterzens/mixin/ServerGamePacketListenerImplMixin_BungeeListener.java
+++ common/src/main/java/org/samo_lego/taterzens/mixin/ServerGamePacketListenerImplMixin_BungeeListener.java
@@ -3,11 +3,16 @@ package org.samo_lego.taterzens.mixin;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.samo_lego.taterzens.compatibility.BungeeCompatibility;
+import org.samo_lego.taterzens.compatibility.LoaderSpecific;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -23,11 +28,18 @@ import static org.samo_lego.taterzens.compatibility.BungeeCompatibility.AVAILABL
  */
 @Mixin(value = ServerGamePacketListenerImpl.class)
 public class ServerGamePacketListenerImplMixin_BungeeListener {
+    @Shadow public ServerPlayer player;
+
+    @Unique
+    private static final String taterzens$permission = "taterzens.npc.edit.commands.addBungee";
+
     @Inject(method = "handleCustomPayload", at = @At("TAIL"))
     private void onCustomPayload(ServerboundCustomPayloadPacket packet, CallbackInfo ci) {
         ResourceLocation packetId = packet.getIdentifier();
+        CommandSourceStack commandSourceStack = player.createCommandSourceStack();
+        boolean hasPermission = LoaderSpecific.permissions$checkPermission(commandSourceStack, taterzens$permission, config.perms.npcCommandPermissionLevel);
 
-        if(AVAILABLE_SERVERS.isEmpty() && config.bungee.enableCommands) {
+        if(AVAILABLE_SERVERS.isEmpty() && config.bungee.enableCommands && hasPermission) {
             if(packetId.equals(BungeeCompatibility.BUNGEE_CHANNEL)) {
                 // Reading data
                 byte[] bytes = new byte[packet.getData().readableBytes()];
