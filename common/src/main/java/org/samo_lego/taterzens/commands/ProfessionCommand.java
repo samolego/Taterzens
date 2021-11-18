131429fd8be1ba3282cb102e934e651163bb89e8
samolego <34912839+samolego@users.noreply.github.com>
2021-11-18 18:08:00 +0100

    Fix null reference in profession command

--- common/src/main/java/org/samo_lego/taterzens/commands/ProfessionCommand.java
+++ common/src/main/java/org/samo_lego/taterzens/commands/ProfessionCommand.java
@@ -9,16 +9,22 @@ import static org.samo_lego.taterzens.Taterzens.config;
 import static org.samo_lego.taterzens.compatibility.LoaderSpecific.permissions$checkPermission;
 
 public class ProfessionCommand {
-    public static LiteralCommandNode<CommandSourceStack> PROFESSION_COMMAND_NODE;
+    public static final LiteralCommandNode<CommandSourceStack> PROFESSION_COMMAND_NODE;
 
     /**
-     * Registers "/trait" node. Can be used to manage scarpet professions.
+     * Registers "/profession" node. Can be used to manage scarpet professions,
+     * or other professions if they hook into the node.
      * @param dispatcher command dispatcher
      * @param dedicated whether server is dedicated or singleplayer.
      */
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher, boolean dedicated) {
-        PROFESSION_COMMAND_NODE = dispatcher.register(literal("profession")
-                .requires(src -> permissions$checkPermission(src, "taterzens.profession", config.perms.traitCommandPermissionLevel))
-        );
+        dispatcher.getRoot().addChild(PROFESSION_COMMAND_NODE);
+    }
+
+    static {
+        PROFESSION_COMMAND_NODE = literal("profession")
+                    .requires(src -> permissions$checkPermission(src, "taterzens.profession", config.perms.traitCommandPermissionLevel)
+                )
+                .build();
     }
 }
