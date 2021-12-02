c9cec4e8a0bcc4448c9152088f7f00f8b512163f
samolego <34912839+samolego@users.noreply.github.com>
2021-12-02 20:15:30 +0100

    Add respawn ability

---
+++ common/src/main/java/org/samo_lego/taterzens/commands/edit/RespawnPointCommand.java
@@ -0,0 +1,58 @@
+package org.samo_lego.taterzens.commands.edit;
+
+import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.phys.Vec3;
+import org.samo_lego.taterzens.commands.NpcCommand;
+
+import static net.minecraft.commands.Commands.argument;
+import static net.minecraft.commands.Commands.literal;
+import static org.samo_lego.taterzens.Taterzens.config;
+import static org.samo_lego.taterzens.compatibility.LoaderSpecific.permissions$checkPermission;
+import static org.samo_lego.taterzens.util.TextUtil.successText;
+
+public class RespawnPointCommand {
+    public static void registerNode(LiteralCommandNode<CommandSourceStack> editNode) {
+        LiteralCommandNode<CommandSourceStack> respawnNode = literal("respawn")
+                .requires(src -> permissions$checkPermission(src, "taterzens.npc.edit.respawn", config.perms.npcCommandPermissionLevel))
+                .then(literal("setCoordinates")
+                        .requires(src -> permissions$checkPermission(src, "taterzens.npc.edit.respawn.coordinates", config.perms.npcCommandPermissionLevel))
+                        .then(argument("coordinates", BlockPosArgument.blockPos())
+                                .executes(RespawnPointCommand::setRespawnCoords)
+                        )
+                )
+                .then(literal("toggle")
+                        .requires(src -> permissions$checkPermission(src, "taterzens.npc.edit.respawn.toggle", config.perms.npcCommandPermissionLevel))
+                        .then(argument("do respawn", BoolArgumentType.bool())
+                                .executes(RespawnPointCommand::toggleRespawn)
+                        )
+                )
+                .build();
+
+        editNode.addChild(respawnNode);
+    }
+
+    private static int toggleRespawn(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        CommandSourceStack src = context.getSource();
+        Vec3 respawnPos = BoolArgumentType.getBool(context, "do respawn") ? context.getSource().getPosition() : null;
+        return NpcCommand.selectedTaterzenExecutor(src.getEntityOrException(), taterzen -> {
+            taterzen.setRespawnPos(respawnPos);
+            src.sendSuccess(successText("taterzens.command.respawn.toggle", String.valueOf(respawnPos == null)), false);
+        });
+
+    }
+
+    private static int setRespawnCoords(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        CommandSourceStack src = context.getSource();
+        BlockPos respawnPos = BlockPosArgument.getSpawnablePos(context, "coordinates");
+        return NpcCommand.selectedTaterzenExecutor(src.getEntityOrException(), taterzen -> {
+            taterzen.setRespawnPos(Vec3.atCenterOf(respawnPos));
+            src.sendSuccess(successText("taterzens.command.respawn.coordinates", respawnPos.toShortString()), false);
+        });
+    }
+}
