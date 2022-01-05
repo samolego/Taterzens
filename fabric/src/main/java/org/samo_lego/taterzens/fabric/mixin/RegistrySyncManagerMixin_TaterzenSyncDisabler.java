6b8e62929f4c5f5acf8c3faf344fc7c7b6bb0f4a
samolego <34912839+samolego@users.noreply.github.com>
2022-01-05 22:01:07 +0100

    Fix crash with new FAPI reg sync

--- fabric/src/main/java/org/samo_lego/taterzens/fabric/mixin/RegistrySyncManagerMixin_TaterzenSyncDisabler.java
+++ fabric/src/main/java/org/samo_lego/taterzens/fabric/mixin/RegistrySyncManagerMixin_TaterzenSyncDisabler.java
@@ -1,12 +1,16 @@
 package org.samo_lego.taterzens.fabric.mixin;
 
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Map;
 
 import static org.samo_lego.taterzens.Taterzens.MODID;
 
@@ -14,21 +18,27 @@ import static org.samo_lego.taterzens.Taterzens.MODID;
 @Mixin(RegistrySyncManager.class)
 public class RegistrySyncManagerMixin_TaterzenSyncDisabler {
 
+    private static final ResourceLocation ENTITY_TYPE = new ResourceLocation("minecraft", "entity_type");
+
     /**
      * Removes taterzen tag from registry sync, as we do not need it on client.
      * Prevents client from being kicked if using FAPI.
      */
     @Inject(
-            method = "toTag",
+            method = "createAndPopulateRegistryMap",
             at = @At("TAIL"),
             locals = LocalCapture.CAPTURE_FAILHARD,
             remap = false
     )
-    private static void removeTaterzenFromSync(boolean isClientSync, CompoundTag activeTag, CallbackInfoReturnable<CompoundTag> cir, CompoundTag mainTag, CompoundTag tag) {
-        CompoundTag registries = tag.getCompound("registries");
-        CompoundTag entityTypes = registries.getCompound("minecraft:entity_type");
-        if(entityTypes != null) {
-            entityTypes.remove(MODID + ":npc");
+    private static void removeTaterzenFromSync(boolean isClientSync,
+                                               @Nullable Map<ResourceLocation,
+                                               Object2IntMap<ResourceLocation>> activeMap,
+                                               CallbackInfoReturnable<Map<ResourceLocation,
+                                               Object2IntMap<ResourceLocation>>> cir,
+                                               Map<ResourceLocation, Object2IntMap<ResourceLocation>> map
+    ) {
+        if (isClientSync) {
+            map.get(ENTITY_TYPE).removeInt(new ResourceLocation(MODID, "npc"));
         }
     }
 }
