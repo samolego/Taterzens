5dd93ea380422b4fe5eba145585b416e4d4dd6aa
altrisi <altrisi.trillosierra@gmail.com>
2022-03-09 20:59:04 +0100

    Simplify Scarpet functions class

--- fabric/src/main/java/org/samo_lego/taterzens/fabric/compatibility/carpet/AdditionalFunctions.java
+++ fabric/src/main/java/org/samo_lego/taterzens/fabric/compatibility/carpet/AdditionalFunctions.java
@@ -6,6 +6,7 @@ import carpet.script.value.EntityValue;
 import carpet.script.value.NullValue;
 import carpet.script.value.Value;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
 import org.samo_lego.taterzens.Taterzens;
 import org.samo_lego.taterzens.api.TaterzensAPI;
 import org.samo_lego.taterzens.interfaces.ITaterzenEditor;
@@ -26,9 +27,9 @@ public class AdditionalFunctions {
      * @param name name of taterzen to create.
      * @return created taterzen.
      */
-    @ScarpetFunction(maxParams = 2)
-    public EntityValue spawn_taterzen(ServerPlayer player, String name) {
-        return (EntityValue) EntityValue.of(TaterzensAPI.createTaterzen(player, name));
+    @ScarpetFunction
+    public Entity spawn_taterzen(ServerPlayer player, String name) {
+        return TaterzensAPI.createTaterzen(player, name);
     }
 
     /**
@@ -36,12 +37,9 @@ public class AdditionalFunctions {
      * @param player player to get taterzen from.
      * @return taterzen of player or null if player doesn't have taterzen selected.
      */
-    @ScarpetFunction(maxParams = 1)
-    public Value players_taterzen(ServerPlayer player) {
-        TaterzenNPC npc = ((ITaterzenEditor) player).getNpc();
-        if (npc != null)
-            return EntityValue.of(npc);
-        return NullValue.NULL;
+    @ScarpetFunction
+    public Entity players_taterzen(ServerPlayer player) {
+        return ((ITaterzenEditor) player).getNpc();
     }
 
     /**
@@ -49,12 +47,12 @@ public class AdditionalFunctions {
      * @param id id of taterzen to get.
      * @return taterzen with given id or null if taterzen with given id doesn't exist.
      */
-    @ScarpetFunction(maxParams = 1)
-    public Value taterzen_by_id(int id) {
+    @ScarpetFunction
+    public Entity taterzen_by_id(int id) {
         // Check size of TATERZEN_NPCS
         if (id < Taterzens.TATERZEN_NPCS.size()) {
-            return EntityValue.of((TaterzenNPC) Taterzens.TATERZEN_NPCS.toArray()[id]);
+            return (TaterzenNPC) Taterzens.TATERZEN_NPCS.toArray()[id];
         }
-        return NullValue.NULL;
+        return null;
     }
 }
