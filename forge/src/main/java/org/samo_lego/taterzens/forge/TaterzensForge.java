0485ddbb1abd1b09f800483302eb7f4642524ba2
samolego <34912839+samolego@users.noreply.github.com>
2022-07-26 10:55:17 +0200

    Minor cleanup

--- forge/src/main/java/org/samo_lego/taterzens/forge/TaterzensForge.java
+++ forge/src/main/java/org/samo_lego/taterzens/forge/TaterzensForge.java
@@ -1,7 +1,6 @@
 package org.samo_lego.taterzens.forge;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -10,16 +9,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 import org.samo_lego.taterzens.Taterzens;
 import org.samo_lego.taterzens.forge.event.EventHandler;
 import org.samo_lego.taterzens.forge.platform.ForgePlatform;
-import org.samo_lego.taterzens.npc.TaterzenNPC;
 
 import static org.samo_lego.taterzens.Taterzens.MOD_ID;
-import static org.samo_lego.taterzens.Taterzens.NPC_ID;
-import static org.samo_lego.taterzens.Taterzens.TATERZEN_TYPE;
 
 @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 @Mod(MOD_ID)
 public class TaterzensForge {
-    private static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, MOD_ID);
+    public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, MOD_ID);
 
     public TaterzensForge() {
         var evtBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -27,12 +23,5 @@ public class TaterzensForge {
         ENTITIES.register(evtBus);
 
         new Taterzens(new ForgePlatform());
-    }
-
-    static {
-        TATERZEN_TYPE = ENTITIES.register(NPC_ID.getPath(), () -> EntityType.Builder
-                .<TaterzenNPC>of(TaterzenNPC::new, MobCategory.MISC)
-                .sized(0.6F, 1.8F)
-                .build(NPC_ID.getPath()));
     }
 }
--- forge/src/main/java/org/samo_lego/taterzens/forge/platform/ForgePlatform.java
+++ forge/src/main/java/org/samo_lego/taterzens/forge/platform/ForgePlatform.java
@@ -3,16 +3,20 @@ package org.samo_lego.taterzens.forge.platform;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.registries.RegistryManager;
+import org.samo_lego.taterzens.npc.TaterzenNPC;
 import org.samo_lego.taterzens.platform.Platform;
+import org.samo_lego.taterzens.util.TextUtil;
 
 import java.nio.file.Path;
-import java.util.Collections;
 
-import static org.samo_lego.taterzens.commands.NpcCommand.npcNode;
-import static org.samo_lego.taterzens.gui.EditorGUI.createCommandGui;
+import static org.samo_lego.taterzens.Taterzens.NPC_ID;
+import static org.samo_lego.taterzens.Taterzens.TATERZEN_TYPE;
+import static org.samo_lego.taterzens.forge.TaterzensForge.ENTITIES;
 
 public class ForgePlatform extends Platform {
 
@@ -41,12 +45,16 @@ public class ForgePlatform extends Platform {
 
     @Override
     public void registerTaterzenType() {
-        //TATERZEN_TYPE = TATERZEN.get();
+        TATERZEN_TYPE = ENTITIES.register(NPC_ID.getPath(), () -> EntityType.Builder
+                .<TaterzenNPC>of(TaterzenNPC::new, MobCategory.MISC)
+                .sized(0.6F, 1.8F)
+                .build(NPC_ID.getPath()));
     }
 
     @Override
     public void openEditorGui(ServerPlayer player) {
-        var editorGUI = createCommandGui(player, null, npcNode, Collections.singletonList("npc"), false);
-        editorGUI.open();
+        player.sendSystemMessage(TextUtil.translate("taterzens.gui.forge"));
+        /*var editorGUI = createCommandGui(player, null, npcNode, Collections.singletonList("npc"), false);
+        editorGUI.open();*/
     }
 }
