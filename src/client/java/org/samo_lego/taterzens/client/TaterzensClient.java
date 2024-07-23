aad035e81eb14cb44d5365f557975e10699eb491
UntalentedAmateur <73374960+UntalentedAmateur@users.noreply.github.com>
2024-07-23 19:01:59 +1000

    Update TaterzensClient.java
    This just seems to be dead code.  It throws an error at build when not commented out, but doesn't seem to affect mod function.  Most of the imports can be removed, too.

--- src/client/java/org/samo_lego/taterzens/client/TaterzensClient.java
+++ src/client/java/org/samo_lego/taterzens/client/TaterzensClient.java
@@ -14,11 +14,13 @@ public class TaterzensClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// We just do this to avoid client crashes
+		/*
 		EntityRendererRegistry.register(Taterzens.TATERZEN_TYPE.get(), context -> new MobRenderer<>(context, new PlayerModel<>(context.bakeLayer(ModelLayers.PLAYER), false), 1.0F) {
 			@Override
 			public ResourceLocation getTextureLocation(TaterzenNPC entity) {
 				return DefaultPlayerSkin.getDefaultTexture();
 			}
 		});
+		*/
 	}
 }
